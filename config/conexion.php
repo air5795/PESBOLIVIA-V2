4d72a38901941d4a9d5eee04975d38fe6a95c84f
alejandroigles <air5795@gmail.com>
2026-05-02 13:47:12 -0400

    Habilitar errores para diagnóstico

--- config/conexion.php
+++ config/conexion.php
@@ -32,10 +32,10 @@ if ($entorno == 'local') {
     $db_password = '71811452Ale*';
     $db_name = 'airsoftb_pesbolivia-v2';
     
-    // Ocultar errores en producción
-    ini_set('display_errors', 0);
-    ini_set('display_startup_errors', 0);
-    error_reporting(0);
+    // Mostrar errores temporalmente para diagnóstico
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
 }
 
 // Establecer conexión con manejo de errores mejorado
